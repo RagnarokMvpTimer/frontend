c86c0f142d6ed5294a58b1a092dbc511fc4d56b6
Lucas8x <lucasemanuel8x@gmail.com>
2022-03-01 15:58:27 -0300

    add tablet media query

--- src/utils/media.ts
+++ src/utils/media.ts
@@ -1,5 +1,11 @@
 import { css } from 'styled-components';
 
+export const tablet = (inner: any) => css`
+  @media (max-width: 768px) {
+    ${inner}
+  }
+`;
+
 export const mobile = (inner: any) => css`
   @media (max-width: ${1000 / 16}em) {
     ${inner};
