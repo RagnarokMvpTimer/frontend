1e723484b8ab2c8dc31b72f6c3dd1952607653c5
Lucas8x <lucasemanuel8x@gmail.com>
2022-03-20 12:51:26 -0300

    docstring test

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -12,6 +12,17 @@ export const respawnCountdown = (time: Moment) => time.format('HH:mm:ss');
 export const respawnAt = (time: Moment) =>
   `${time.format('HH:mm')} ~ ${time.add(10, 'm').format('HH:mm')}`;
 
-export const getMvpSprite = (id: number) => mvpIcons[id] || Question;
+/**
+ * Returns the MVP sprite
+ * @param id mvp id
+ * @returns the sprite of the mvp
+ */
+export const getMvpSprite = (id: number): string => mvpIcons[id] || Question;
 
-export const getMapImg = (mapname: string) => mvpMaps[mapname] || null;
+/**
+ * Returns the MVP map image
+ * @param mapname name of the map
+ * @returns the sprite of the map
+ */
+export const getMapImg = (mapname: string): string =>
+  mvpMaps[mapname] || Question;
