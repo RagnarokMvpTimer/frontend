44f07a1774eb6906bce79e382e59a1710cd08586
Lucas8x <lucasemanuel8x@gmail.com>
2022-04-09 21:40:31 -0300

    add footer information

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,40 @@
-import { FooterContainer } from './styles';
+import { Github, ExternalLink } from '@styled-icons/feather';
+
+import { FooterContainer, Main, Text, Bold, Link } from './styles';
 
 export function Footer() {
-  return <FooterContainer></FooterContainer>;
+  return (
+    <FooterContainer>
+      <Main>
+        <Text>
+          Made with ❤️ by{' '}
+          <Link href='https://github.com/Lucas8x' target='_blank'>
+            Lucas Emanuel <Github size={14} />
+          </Link>
+        </Text>
+
+        <Text>
+          Source code avaliable on{' '}
+          <Link
+            href='https://github.com/RagnarokMvpTimer/frontend'
+            target='_blank'
+          >
+            Github <Github size={14} />
+          </Link>
+        </Text>
+
+        <Text>
+          Sprites from{' '}
+          <Link href='https://www.divine-pride.net/' target='_blank'>
+            DivinePride <ExternalLink size={14} />
+          </Link>
+        </Text>
+
+        <Text>
+          This site is not affiliated with <Bold>DivinePride</Bold> or{' '}
+          <Bold>Gravity Interactive</Bold>
+        </Text>
+      </Main>
+    </FooterContainer>
+  );
 }
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -8,3 +8,37 @@ export const FooterContainer = styled.footer`
 
   background-color: ${({ theme }) => theme.colors.primary};
 `;
+
+export const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+
+  margin: 15px auto;
+`;
+
+export const Text = styled.span`
+  display: flex;
+  flex-direction: row;
+  gap: 5px;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+export const Bold = styled.span`
+  font-weight: 500;
+`;
+
+export const Link = styled.a`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 5px;
+
+  text-decoration: none;
+  color: #ffa800;
+`;
