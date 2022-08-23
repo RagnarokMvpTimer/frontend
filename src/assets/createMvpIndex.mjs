baf4891e3fbcaec0825aa51cb64cf709e327e1ed
Lucas8x <lucasemanuel8x@gmail.com>
2022-08-22 22:56:17 -0300

    add missing animated mvp sprites

--- src/assets/createMvpIndex.mjs
+++ src/assets/createMvpIndex.mjs
@@ -5,11 +5,16 @@ import path from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const VALID_EXTENSIONS = ['png', 'gif'];
+
 function createIndex(folderName, exportName) {
   const imagesDir = path.resolve(__dirname, folderName);
-  const files = readdirSync(imagesDir).filter((img) => img.includes('png'));
+  const files = readdirSync(imagesDir).filter((img) => {
+    const ext = img.split('.')[1].toLowerCase();
+    return VALID_EXTENSIONS.includes(ext);
+  });
 
-  const item = (file) => `"${file.split('.png')[0]}": require("./${file}")`;
+  const item = (file) => `"${file.split('.')[0]}": require("./${file}")`;
 
   const ex = files.map(item).join(',\n  ');
 
