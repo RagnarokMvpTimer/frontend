f46a4af36823097bab1e1ed33ff1b8f7a3acb044
Lucas8x <lucasemanuel8x@gmail.com>
2024-01-09 17:57:54 -0300

    build: update to vite 5
    + stringify json

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { imagetools } from 'vite-imagetools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  json: {
+    stringify: true,
+  },
   resolve: {
     alias: [
       {
